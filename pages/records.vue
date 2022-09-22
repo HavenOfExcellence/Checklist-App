5d32a9c9393312053e666b1bb4e09800f9083258
chuahziyang <czyang2002@gmail.com>
2022-09-22 18:36:33 +0800

    asd

--- pages/records.vue
+++ pages/records.vue
@@ -3,6 +3,14 @@
 <template>
   <view-wrapper title="Records">
     <div class="h-screen">
+      <a
+        href="https://docs.google.com/spreadsheets/d/1XeZxdwWQVwSsp20qUcP-JkpfhJrPPS6is7DH0yy3pHg/edit?usp=sharing"
+        target="_"
+      >
+        <h2 class="text-sm text-blue-700 underline mb-6">
+          Click to see Sheet in a new Tap
+        </h2>
+      </a>
       <iframe
         class="h-2/3 w-full"
         src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQGNRuBU_nCukhqY_RwVNhzPOGkiqqkPJeBzadlT_TVZfbQYHSKvt7hnIsfw44QVDLWPpbXZSQw0hSc/pubhtml?widget=true&amp;headers=false"
